5ed3555be214f9543ee1e004ddbbba632f3ab6cd
Boian Ivanov <boian.ivanov@itech.media>
2022-09-19 15:58:08 +0100

    add: button to redirect to game

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-<h1>Foo</h1>
+    <div class="w-full h-full flex flex-col justify-center items-center relative z-20">
+        <div class="flex flex-col mb-1">
+            <span class="text-indigo-500 text-4xl text-center mb-4">2048</span>
+            <nuxt-link
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                to="/game"
+            >
+                Play Single player
+            </nuxt-link>
+        </div>
+    </div>
 </template>
 
 <script>
